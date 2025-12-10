50138725efd073a85c23e52bfee797c1290266e7
Siri Undeland Haugen <siriundeland.haugen@skatteetaten.no>
2025-12-10 14:05:45 +0100

    added some files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-function showAnswer(response) {
+function showPoem(response) {
   alert(response.data.answer);
+  
 }
 
-let prompt = "What is the name of the first femail president in the USA";
-let context = "Make sure to provide a very short and clear answer";
+let prompt = "Make short rhymes and poems about Norway.";
+let context = "Make short, funny and amusing poems or rhymes about the Norwegian culture and people.";
 let apiKey = "df32e8cdob8b09f2660834841061ata2";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-axios.get(apiUrl).then(showAnswer);
+axios.get(apiUrl).then(showPoem);
