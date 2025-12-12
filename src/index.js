0a2080dc5a27eae52edb54edd520b7b829e8d694
Siri Undeland Haugen <siriundeland.haugen@skatteetaten.no>
2025-12-12 12:26:13 +0100

    Added some css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 function showPoem(response) {
-  alert(response.data.answer);
-  
-}
+  //alert(response.data.answer);
+  document.querySelector("#poem-output").innerHTML = response.data.answer;
 
 let prompt = "Make short rhymes and poems about Norway.";
 let context = "Make short, funny and amusing poems or rhymes about the Norwegian culture and people.";
 let apiKey = "df32e8cdob8b09f2660834841061ata2";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-axios.get(apiUrl).then(showPoem);
+axios.get(apiUrl).then(showPoem)};
