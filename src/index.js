8d0f9ed91331df5c23a8bb94caa5f13336b3fa51
Siri Undeland Haugen <siriundeland.haugen@skatteetaten.no>
2025-12-12 13:37:12 +0100

    added typewriter in javascript

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-function showPoem(response) {
-  //alert(response.data.answer);
+/*function showPoem(response) {
   document.querySelector("#poem-output").innerHTML = response.data.answer;
 
 let prompt = "Make short rhymes and poems about Norway.";
@@ -8,3 +7,20 @@ let apiKey = "df32e8cdob8b09f2660834841061ata2";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
 axios.get(apiUrl).then(showPoem)};
+*/
+
+
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "De tre bukkene Bruse skulle gå over broen",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
