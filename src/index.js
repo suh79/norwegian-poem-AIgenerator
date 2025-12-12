3d99886d8c513fdda04b4030901a7188ebaab732
Siri Undeland Haugen <siriundeland.haugen@skatteetaten.no>
2025-12-12 14:35:28 +0100

    using AI

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
-/*function showPoem(response) {
-  document.querySelector("#poem-output").innerHTML = response.data.answer;
-
-let prompt = "Make short rhymes and poems about Norway.";
-let context = "Make short, funny and amusing poems or rhymes about the Norwegian culture and people.";
-let apiKey = "df32e8cdob8b09f2660834841061ata2";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-axios.get(apiUrl).then(showPoem)};
-*/
-
-
-
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) { 
+  console.log("Poem generated");
 
   new Typewriter("#poem", {
-    strings: "De tre bukkene Bruse skulle gå over broen",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
+function generatePoem(event) {
+   event.preventDefault();
+
+let instructionInput = document.querySelector("#user-instructions");
+let apiKey = "df32e8cdob8b09f2660834841061ata2";
+let prompt = `User instructions: Generate a Norwegian poem about ${instructionInput.value}`;
+let context = "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Do not list HTML in the pome. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+// kall apiet og vis diktet
+
+  //let poemElement = document.querySelector("#poem");
+  //poemElement.classList.remove("hidden");
+  //poemElement.innerHTML = `<div class="generating">⏳ Generating a Norwegian poem about ${instructionsInput.value}</div>`;
+
+axios.get(apiUrl).then(displayPoem)
+};
+ 
+ let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
