2b50b8398367951b000d9884c276563bde56caa3
Siri Undeland Haugen <siriundeland.haugen@skatteetaten.no>
2025-12-12 15:22:50 +0100

    Adding UX

--- src/index.js
+++ src/index.js
@@ -12,16 +12,16 @@ function displayPoem(response) {
 function generatePoem(event) {
    event.preventDefault();
 
-let instructionInput = document.querySelector("#user-instructions");
+let instructionsInput = document.querySelector("#user-instructions");
 let apiKey = "df32e8cdob8b09f2660834841061ata2";
-let prompt = `User instructions: Generate a Norwegian poem about ${instructionInput.value}`;
+let prompt = `User instructions: Generate a Norwegian poem about ${instructionsInput.value}`;
 let context = "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Do not list HTML in the pome. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 // kall apiet og vis diktet
 
-  //let poemElement = document.querySelector("#poem");
-  //poemElement.classList.remove("hidden");
-  //poemElement.innerHTML = `<div class="generating">⏳ Generating a Norwegian poem about ${instructionsInput.value}</div>`;
+let poemElement = document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = `<div class="generating">⏳ Generating a Norwegian poem about ${instructionsInput.value}</div>`;
 
 axios.get(apiUrl).then(displayPoem)
 };
